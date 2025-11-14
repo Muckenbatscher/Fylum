d4abf4a0e86e3ef5b1aa698b5fbcbfe9805abc00
Muckenbatscher <christi.braeuner97@gmail.com>
2025-11-14 11:22:25 +0100

    add http command to perform migrations

--- Fylum.AppHost/AppHost.cs
+++ Fylum.AppHost/AppHost.cs
@@ -1,3 +1,7 @@
+using Fylum.Migrations.Api.Shared;
+
+namespace Fylum.AppHost;
+
 internal class Program
 {
     private static void Main(string[] args)
@@ -8,8 +12,48 @@ internal class Program
             .WithEnvironment("POSTGRES_DB", "fylum")
             .AddDatabase("fylum");
 
-        var api = builder.AddProject<Projects.Fylum_Api>("api");
+        var migrationPerformingKey = builder.AddParameter("MigrationPerformingKey", secret: true);
 
-        builder.Build().Run();
+        var api = builder.AddProject<Projects.Fylum_Api>("api")
+            .WithHttpCommand(
+            path: EndpointRoutes.MigrationsPerformMinimallyRequiredRoute,
+            displayName: "Perform Minimally Required Migrations",
+            commandOptions: new HttpCommandOptions()
+            {
+                Description = """
+                Migrates the database to the minimally required state.
+                The migrations and users contexts are ensured to exist.
+                """,
+
+                PrepareRequest = (context) =>
+                {
+                    var key = migrationPerformingKey.Resource.GetValueAsync(context.CancellationToken);
+                    context.Request.Headers.Add("X-MigrationPerforming-Key", $"Key: {key}");
+                    return Task.CompletedTask;
+                },
+                IconName = "DatabaseLightningRegular"
+            })
+            .WithHttpCommand(
+            path: EndpointRoutes.MigrationsPerformAllRoute,
+            displayName: "Perform All Available Migrations",
+            commandOptions: new HttpCommandOptions()
+            {
+                Description = """
+                 Migrates the database to the latest available state.
+                 All the contexts are ensured to exist.
+                 """,
+
+                PrepareRequest = (context) =>
+                {
+                    var key = migrationPerformingKey.Resource.GetValueAsync(context.CancellationToken);
+                    context.Request.Headers.Add("X-MigrationPerforming-Key", $"Key: {key}");
+                    return Task.CompletedTask;
+                },
+                IconName = "DatabaseCheckmarkRegular"
+            });
+
+        var app = builder.Build();
+
+        app.Run();
     }
 }
