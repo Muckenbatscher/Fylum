75006a5244bc2dc392def821646f0384439f3246
Muckenbatscher <christi.braeuner97@gmail.com>
2025-11-16 12:39:44 +0100

    refactor the methods to add the miration commands to a separate extension method update build props to target net 10

--- Fylum.AppHost/AppHost.cs
+++ Fylum.AppHost/AppHost.cs
@@ -1,4 +1,3 @@
-using Aspire.Hosting;
 using Fylum.Migrations.Api.Shared;
 
 namespace Fylum.AppHost;
@@ -16,53 +15,17 @@ internal class Program
             configureContainer: pgAdminResource =>
             {
                 pgAdminResource
-                    .WithEnvironment("PGADMIN_CONFIG_SERVER_MODE", "False")
-                    .WithEnvironment("PGADMIN_CONFIG_MASTER_PASSWORD_REQUIRED", "False")
                     .WithVolume(target: "/var/lib/pgadmin", name: "pgadmin_data");
                  });
         var database = postgres.AddDatabase("fylum");
 
         var migrationPerformingKey = builder.AddParameter("MigrationPerformingKey", secret: true);
 
-        string performingKeyHeader = "X-MigrationPerforming-Key";
         var api = builder.AddProject<Projects.Fylum_Api>("api")
             .WithReference(database, "postgres")
             .WaitFor(database)
             .WithChildRelationship(migrationPerformingKey)
-            .WithHttpCommand(
-            path: EndpointRoutes.MigrationsPerformMinimallyRequiredRoute,
-            displayName: "Perform Minimally Required Migrations",
-            commandOptions: new HttpCommandOptions()
-            {
-                Description = """
-                Migrates the database to the minimally required state.
-                The migrations and users contexts are ensured to exist.
-                """,
-                PrepareRequest = (context) =>
-                {
-                    var key = migrationPerformingKey.Resource.GetValueAsync(context.CancellationToken);
-                    context.Request.Headers.Add(performingKeyHeader, $"Key: {key}");
-                    return Task.CompletedTask;
-                },
-                IconName = "DatabaseLightning"
-            })
-            .WithHttpCommand(
-            path: EndpointRoutes.MigrationsPerformAllRoute,
-            displayName: "Perform All Available Migrations",
-            commandOptions: new HttpCommandOptions()
-            {
-                Description = """
-                 Migrates the database to the latest available state.
-                 All the contexts are ensured to exist.
-                 """,
-                PrepareRequest = (context) =>
-                {
-                    var key = migrationPerformingKey.Resource.GetValueAsync(context.CancellationToken);
-                    context.Request.Headers.Add(performingKeyHeader, $"Key: {key}");
-                    return Task.CompletedTask;
-                },
-                IconName = "DatabaseCheckmark"
-            });
+            .WithMigrationCommands(migrationPerformingKey);
 
         var app = builder.Build();
 
