a2e6b61ca153a0ebbe2daae00f1ab8fda69f901a
Muckenbatscher <christi.braeuner97@gmail.com>
2025-12-06 00:58:09 +0100

    added domain model for a refresh token

---
+++ Fylum.Users.Domain/RefreshToken/RefreshToken.cs
@@ -0,0 +1,38 @@
+﻿using Fylum.Domain;
+
+namespace Fylum.Users.Domain.RefreshToken;
+
+public class RefreshToken : IdentifiableEntity<Guid>
+{
+    public Guid UserId { get; }
+    public DateTimeOffset IssuedAt { get; private set; }
+    public DateTimeOffset ExpiredAt { get; private set; }
+
+    public bool IsValid
+        => DateTimeOffset.UtcNow < ExpiredAt && DateTimeOffset.UtcNow > IssuedAt;
+
+    private RefreshToken(Guid id, Guid userId, DateTimeOffset issuedAt, DateTimeOffset expiredAt) : base(id)
+    {
+        UserId = userId;
+        IssuedAt = issuedAt;
+        ExpiredAt = expiredAt;
+    }
+
+    public static RefreshToken IssueNew(Guid userId, TimeSpan validFor)
+    {
+        var id = Guid.NewGuid();
+        var issuedAt = DateTimeOffset.UtcNow;
+        var expiredAt = issuedAt.Add(validFor);
+        return new RefreshToken(id, userId, issuedAt, expiredAt);
+    }
+
+    public static RefreshToken Create(Guid id, Guid userId, DateTimeOffset issuedAt, DateTimeOffset expiredAt)
+    {
+        return new RefreshToken(id, userId, issuedAt, expiredAt);
+    }
+
+    public void Invalidate()
+    {
+        ExpiredAt = DateTimeOffset.UtcNow;
+    }
+}
