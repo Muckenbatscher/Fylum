036b1c424039848f5661c3387abed60730452fe1
Muckenbatscher <christi.braeuner97@gmail.com>
2025-12-11 23:11:01 +0100

    validate that the jwt is still valid before trying to send also add typed exceptions

---
+++ Fylum.Client/Auth/Token/RefreshTokenExpiredException.cs
@@ -0,0 +1,8 @@
+﻿namespace Fylum.Client.Auth.Token;
+
+public class RefreshTokenExpiredException : Exception
+{
+    public RefreshTokenExpiredException() : base("Refresh token is expired. Explicitly Login again first.")
+    {
+    }
+}
