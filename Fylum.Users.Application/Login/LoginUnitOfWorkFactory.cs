cb6d8f80aaab9f0651bf67a4198346b833ebaaf3
Muckenbatscher <christi.braeuner97@gmail.com>
2025-12-15 00:43:46 +0100

    create generic unit of work factory use reflection to determine constructor and resolve scoped dependency services

--- Fylum.Users.Application/Login/LoginUnitOfWorkFactory.cs
+++ Fylum.Users.Application/Login/LoginUnitOfWorkFactory.cs
@@ -1,28 +1,12 @@
 ﻿using Fylum.Application;
 using Fylum.Users.Domain.Login;
-using Fylum.Users.Domain.Password;
-using Fylum.Users.Domain.RefreshTokens;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Fylum.Users.Application.Login;
 
-public class LoginUnitOfWorkFactory : UnitOfWorkFactory, ILoginUnitOfWorkFactory
+public class LoginUnitOfWorkFactory : UnitOfWorkFactory<LoginUnitOfWork>, ILoginUnitOfWorkFactory
 {
     public LoginUnitOfWorkFactory(IServiceScopeFactory serviceScopeFactory) : base(serviceScopeFactory)
     {
-    }
-
-    public LoginUnitOfWork Create()
-    {
-        CreateScope();
-
-        var transactionFactory = GetTransactionFactory();
-        var userRepository = GetScopedService<IUserWithPasswordRepository>();
-        var refreshTokenRepository = GetScopedService<IRefreshTokenRepository>();
-
-        return new LoginUnitOfWork(
-            transactionFactory,
-            userRepository,
-            refreshTokenRepository);
     }
 }
--- Fylum.Users.Application/Register/UserRegisterUnitOfWorkFactory.cs
+++ Fylum.Users.Application/Register/UserRegisterUnitOfWorkFactory.cs
@@ -1,29 +1,13 @@
 ﻿using Fylum.Application;
-using Fylum.Users.Domain.Password;
-using Fylum.Users.Domain.RefreshTokens;
 using Fylum.Users.Domain.Register;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Fylum.Users.Application.Register;
 
-public class UserRegisterUnitOfWorkFactory : UnitOfWorkFactory, IUserRegisterUnitOfWorkFactory
+public class UserRegisterUnitOfWorkFactory : UnitOfWorkFactory<UserRegisterUnitOfWork>, IUserRegisterUnitOfWorkFactory
 {
     public UserRegisterUnitOfWorkFactory(IServiceScopeFactory serviceScopeFactory) :
         base(serviceScopeFactory)
     {
-    }
-
-    public UserRegisterUnitOfWork Create()
-    {
-        CreateScope();
-
-        var transactionFactory = GetTransactionFactory();
-        var userRepository = GetScopedService<IUserWithPasswordRepository>();
-        var refreshTokenRepository = GetScopedService<IRefreshTokenRepository>();
-
-        return new UserRegisterUnitOfWork(
-            transactionFactory,
-            userRepository,
-            refreshTokenRepository);
     }
 }
