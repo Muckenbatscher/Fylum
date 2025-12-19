91fb7a409f0e07882df118029b8ff86aa1ccebce
Muckenbatscher <christi.braeuner97@gmail.com>
2025-12-19 08:43:35 +0100

    added cancellation token overloads in fylum client

--- Fylum.Client/Auth/IAuthClient.cs
+++ Fylum.Client/Auth/IAuthClient.cs
@@ -5,5 +5,8 @@ namespace Fylum.Client.Auth;
 public interface IAuthClient
 {
     Task<LoginResponse> LoginAsync(LoginRequest loginRequest, CancellationToken cancellationToken);
+    Task<LoginResponse> LoginAsync(LoginRequest loginRequest);
+
     Task<RegisterResponse> RegisterAsync(RegisterRequest registerRequest, CancellationToken cancellationToken);
+    Task<RegisterResponse> RegisterAsync(RegisterRequest registerRequest);
 }
--- Fylum.Client/Auth/Token/ITokenService.cs
+++ Fylum.Client/Auth/Token/ITokenService.cs
@@ -2,8 +2,14 @@
 
 public interface ITokenService
 {
-    Task<string?> GetAccessTokenAsync(CancellationToken cancellationToken);
+    Task<string?> GetAccessTokenAsync();
+
     Task LoginAsync(string username, string password, CancellationToken cancellationToken);
+    Task LoginAsync(string username, string password);
+
     Task LogoutAsync(CancellationToken cancellationToken);
+    Task LogoutAsync();
+
     Task RefreshTokenAsync(CancellationToken cancellationToken);
+    Task RefreshTokenAsync();
 }
