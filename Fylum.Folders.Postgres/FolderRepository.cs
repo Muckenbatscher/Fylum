b7c4bf1494ab34ac2018c7688c05210d5565db72
Muckenbatscher <christi.braeuner97@gmail.com>
2025-12-24 13:58:53 +0100

    implemented the rest of folder repository

--- Fylum.Folders.Postgres/FolderRepository.cs
+++ Fylum.Folders.Postgres/FolderRepository.cs
@@ -26,7 +26,49 @@ public class FolderRepository : IFolderRepository
         var transaction = _transactionFactory.GetTransaction();
         transaction.Connection.Execute(sql, param, transaction.Transaction);
     }
-    public Folder? GetById(Guid id) => throw new NotImplementedException();
-    public IEnumerable<Folder> GetChildFolders(Guid parentFolderId) => throw new NotImplementedException();
-    public void Update(Folder folder) => throw new NotImplementedException();
+    public Folder? GetById(Guid id)
+    {
+        var param = new { Id = id };
+        var sql = @$"SELECT id as '{nameof(FolderQueryModel.Id)}', 
+                            parent_folder_id as '{nameof(FolderQueryModel.ParentFolderId)}', 
+                            name as '{nameof(FolderQueryModel.Name)}'
+                     FROM folders
+                     WHERE id = @{nameof(param.Id)};";
+        var transaction = _transactionFactory.GetTransaction();
+        var queryResult = transaction.Connection.QuerySingleOrDefault<FolderQueryModel>(
+            sql, param, transaction.Transaction);
+        return queryResult == null
+            ? null
+            : GetFromQueryModel(queryResult);
+    }
+    public IEnumerable<Folder> GetChildFolders(Guid parentFolderId)
+    {
+        var param = new { ParentFolderId = parentFolderId };
+        var sql = @$"SELECT id as '{nameof(FolderQueryModel.Id)}', 
+                            parent_folder_id as '{nameof(FolderQueryModel.ParentFolderId)}', 
+                            name as '{nameof(FolderQueryModel.Name)}'
+                     FROM folders
+                     WHERE parent_folder_id = @{nameof(param.ParentFolderId)};";
+        var transaction = _transactionFactory.GetTransaction();
+        var result = transaction.Connection.Query<FolderQueryModel>(sql, param, transaction.Transaction);
+        return result.Select(GetFromQueryModel).ToList();
+    }
+    public void Update(Folder folder)
+    {
+        var param = new
+        {
+            folder.Id,
+            folder.ParentFolderId,
+            folder.Name
+        };
+        var sql = @$"UPDATE folders
+                     SET parent_folder_id = @{nameof(FolderQueryModel.ParentFolderId)}, 
+                         name = @{nameof(param.Name)}
+                     WHERE id = @{nameof(param.Id)};";
+        var transaction = _transactionFactory.GetTransaction();
+        transaction.Connection.Execute(sql, param, transaction.Transaction);
+    }
+
+    private Folder GetFromQueryModel(FolderQueryModel model)
+        => Folder.Create(model.Id, model.ParentFolderId, model.Name);
 }
