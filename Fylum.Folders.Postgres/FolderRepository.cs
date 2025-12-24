c6f38fe408fc476dc02d0399730da5e8128a3159
Muckenbatscher <christi.braeuner97@gmail.com>
2025-12-24 13:26:29 +0100

    added implementation of folder repository add

---
+++ Fylum.Folders.Postgres/FolderRepository.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using Fylum.Domain.UnitOfWork;
+using Fylum.Folders.Domain;
+
+namespace Fylum.Folders.Postgres;
+
+public class FolderRepository : IFolderRepository
+{
+    private readonly IUnitOfWorkTransactionFactory _transactionFactory;
+
+    public FolderRepository(IUnitOfWorkTransactionFactory transactionFactory)
+    {
+        _transactionFactory = transactionFactory;
+    }
+
+    public void Add(Folder folder)
+    {
+        var param = new
+        {
+            folder.Id,
+            folder.ParentFolderId,
+            folder.Name
+        };
+        var sql = @$"INSERT INTO folders (id, parent_folder_id, name)
+                     VALUES (@{nameof(param.Id)}, @{nameof(param.ParentFolderId)}, @{nameof(param.Name)});";
+        var transaction = _transactionFactory.GetTransaction();
+        transaction.Connection.Execute(sql, param, transaction.Transaction);
+    }
+    public Folder? GetById(Guid id) => throw new NotImplementedException();
+    public IEnumerable<Folder> GetChildFolders(Guid parentFolderId) => throw new NotImplementedException();
+    public void Update(Folder folder) => throw new NotImplementedException();
+}
