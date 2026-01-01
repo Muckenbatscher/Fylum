c5293156ba77f2a9ea2d4e82a066e9a5fe774fef
Muckenbatscher <christi.braeuner97@gmail.com>
2026-01-01 18:44:35 +0100

    moved the token caching into the main layout to simplify the inheriting pages

--- Fylum.Web/Services/TokenCacheService.cs
+++ Fylum.Web/Services/TokenCacheService.cs
@@ -4,11 +4,5 @@ namespace Fylum.Web.Services;
 
 public class TokenCacheService : ITokenCacheService
 {
-    public TokenCacheService()
-    {
-        Id = Guid.NewGuid();
-        Console.WriteLine($"TokenCacheService created with Id: {Id}");
-    }
-    public Guid Id { get; }
     public TokenPair? CachedToken { get; set; }
 }
