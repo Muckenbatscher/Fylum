50744a7a907512ec9fbfaf0b09286dea292ced07
Muckenbatscher <christi.braeuner97@gmail.com>
2026-02-05 14:48:17 +0100

    added foundation for architecture tests implemented first test for naming QueryHandler and CommandHandler

---
+++ Test/Fylum.Migrations.Api.Architecture.Tests/ApplicationNamingTests.cs
@@ -0,0 +1,34 @@
+﻿using Fylum.Core.Application.Command;
+using Fylum.Core.Application.Query;
+using NetArchTest.Rules;
+
+namespace Fylum.Migrations.Api.Architecture.Tests;
+
+[TestClass]
+public sealed class ApplicationNamingTests
+{
+    [TestMethod]
+    public void CommandHandler_ShouldHave_NameEndingWith_CommandHandler()
+    {
+        var result = Types.InAssembly(typeof(Program).Assembly)
+            .That()
+            .ImplementInterface(typeof(ICommandHandler<>))
+            .Or()
+            .ImplementInterface(typeof(ICommandHandler<,>))
+            .Should()
+            .HaveNameEndingWith("CommandHandler")
+            .GetResult();
+        Assert.IsTrue(result.IsSuccessful, $"Failing types: { string.Join(", ", result.FailingTypes)}");
+    }
+    [TestMethod]
+    public void QueryHandler_ShouldHave_NameEndingWith_QueryHandler()
+    {
+        var result = Types.InAssembly(typeof(Program).Assembly)
+            .That()
+            .ImplementInterface(typeof(IQueryHandler<,>))
+            .Should()
+            .HaveNameEndingWith("QueryHandler")
+            .GetResult();
+        Assert.IsTrue(result.IsSuccessful, $"Failing types: {string.Join(", ", result.FailingTypes)}");
+    }
+}
