c467ac66966fd976c903c77b49a47fdc8dc13969
Muckenbatscher <christi.braeuner97@gmail.com>
2026-02-05 15:49:09 +0100

    added test for naming of the command and query types to match the name of the handlers

--- Test/Fylum.Migrations.Api.Architecture.Tests/ApplicationNamingTests.cs
+++ Test/Fylum.Migrations.Api.Architecture.Tests/ApplicationNamingTests.cs
@@ -1,5 +1,5 @@
-﻿using Fylum.Core.Application.Command;
-using Fylum.Core.Application.Query;
+﻿using Fylum.Core.Application.Query;
+using Fylum.Migrations.Api.Architecture.Tests.CustomRules;
 using NetArchTest.Rules;
 
 namespace Fylum.Migrations.Api.Architecture.Tests;
@@ -7,28 +7,56 @@ namespace Fylum.Migrations.Api.Architecture.Tests;
 [TestClass]
 public sealed class ApplicationNamingTests
 {
+    private PredicateList CommandHandlerImplementations()
+    {
+        return Types.InAssembly(typeof(Program).Assembly)
+            .That()
+            .MeetCustomRule(new CommandHandlerImplementationRule());
+    }
+    private PredicateList QueryHandlerImplementations()
+    {
+        return Types.InAssembly(typeof(Program).Assembly)
+            .That()
+            .AreClasses()
+            .And()
+            .ImplementInterface(typeof(IQueryHandler<,>));
+    }
+
     [TestMethod]
     public void CommandHandler_ShouldHave_NameEndingWith_CommandHandler()
     {
-        var result = Types.InAssembly(typeof(Program).Assembly)
-            .That()
-            .ImplementInterface(typeof(ICommandHandler<>))
-            .Or()
-            .ImplementInterface(typeof(ICommandHandler<,>))
+        var result = CommandHandlerImplementations()
             .Should()
             .HaveNameEndingWith("CommandHandler")
             .GetResult();
-        Assert.IsTrue(result.IsSuccessful, $"Failing types: {string.Join(", ", result.FailingTypes)}");
+        Assert.Successful(result);
     }
+    [TestMethod]
+    public void CommandHandlers_Should_Match_CommandName()
+    {
+        var result = CommandHandlerImplementations()
+            .Should()
+            .MeetCustomRule(new CommandHandlerNameMatchesCommandRule())
+            .GetResult();
+        Assert.Successful(result);
+    }
+
     [TestMethod]
     public void QueryHandler_ShouldHave_NameEndingWith_QueryHandler()
     {
-        var result = Types.InAssembly(typeof(Program).Assembly)
-            .That()
-            .ImplementInterface(typeof(IQueryHandler<,>))
+        var result = QueryHandlerImplementations()
             .Should()
             .HaveNameEndingWith("QueryHandler")
             .GetResult();
-        Assert.IsTrue(result.IsSuccessful, $"Failing types: {string.Join(", ", result.FailingTypes)}");
+        Assert.Successful(result);
+    }
+    [TestMethod]
+    public void QueryHandlers_Should_Match_QueryName()
+    {
+        var result = QueryHandlerImplementations()
+            .Should()
+            .MeetCustomRule(new QueryHandlerNameMatchesQueryRule())
+            .GetResult();
+        Assert.Successful(result);
     }
 }
---
+++ Test/Fylum.Migrations.Api.Architecture.Tests/AssertNetArchResultExtensions.cs
@@ -0,0 +1,14 @@
+﻿using TestResult = NetArchTest.Rules.TestResult;
+
+namespace Fylum.Migrations.Api.Architecture.Tests;
+
+internal static class AssertNetArchResultExtensions
+{
+    extension(Assert)
+    {
+        public static void Successful(TestResult result)
+        {
+            Assert.IsTrue(result.IsSuccessful, $"Failing types: {string.Join(", ", result.FailingTypes)}");
+        }
+    }
+}
