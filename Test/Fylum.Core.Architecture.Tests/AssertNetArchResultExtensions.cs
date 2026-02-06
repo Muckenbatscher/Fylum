0da1af4496924b34bfe65bda7b0f8a8bf6859a5a
Muckenbatscher <christi.braeuner97@gmail.com>
2026-02-06 14:08:25 +0100

    added dependency direction tests for core project added the test project to the build-test workflow in the architecture tests job

---
+++ Test/Fylum.Core.Architecture.Tests/AssertNetArchResultExtensions.cs
@@ -0,0 +1,14 @@
+﻿using TestResult = NetArchTest.Rules.TestResult;
+
+namespace Fylum.Core.Architecture.Tests;
+
+internal static class AssertNetArchResultExtensions
+{
+    extension(TestResult result)
+    {
+        public void AssertSuccessful()
+        {
+            Assert.IsTrue(result.IsSuccessful, $"Failing types: {string.Join(", ", result.FailingTypes)}");
+        }
+    }
+}
